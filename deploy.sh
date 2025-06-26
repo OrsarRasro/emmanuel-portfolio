9b1022a3548587d2a5709d221462450a8f8eff84
OrsarRasro <emmachirokee@gmail.com>
2025-06-26 11:09:12 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+sudo su
+yum update -y
+yum install -y httpd
+cd /var/www/html
+wget https://github.com/OrsarRasro/emmanuel-portfolio/archive/refs/heads/master.zip
+unzip master.zip
+cp -r emmanuel-portfolio-master/* /var/www/html/
+rm -rf emmanuel-portfolio-master master.zip
+systemctl enable httpd 
+systemctl start httpd
